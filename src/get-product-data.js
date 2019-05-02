31d40c035d835f3bada39a031c086427445438bd
AustinLeeGordon <gordon.austin2@gmail.com>
2019-05-02 16:21:04 -0500

    Move from cheerio to jsdom, pull more content

--- src/get-product-data.js
+++ src/get-product-data.js
@@ -1,4 +1,4 @@
-const cheerio = require('cheerio');
+const { JSDOM } = require('jsdom');
 const fetch = require('fetch-retry');
 const he = require('he');
 
@@ -8,16 +8,14 @@ async function getProductData(productUrls, options) {
         let html = await fetch(productUrl, {
             method: 'GET',
             headers
-        });
-        html = await html.text();
+        }).then(res => res.text());
 
-        const $ = cheerio.load(html);
+        let dom = new JSDOM(html);
+        let window = dom.window;
+        let { document } = window;
 
         // Product title
-        let title = $('#productNameContainer span[itemprop=name]').text();
-        if (encodeEntities) {
-            title = encode(title);
-        }
+        let title = document.querySelector('#productNameContainer span[itemprop=name]').textContent;
 
         // Product SKU and categories
         let splitUrl = productUrl.split('/').filter(s => s);
@@ -26,67 +24,157 @@ async function getProductData(productUrls, options) {
         let category = splitUrl.pop().toLowerCase().toLowerCase();
 
         // Product description
-        let description = $('#read-more .modal-body').text() || '';
+        let description = document.querySelector('#read-more .modal-body').textContent || '';
         description = description.replace(/\s\s+/g, ' ').trim();
-        if (encodeEntities) {
-            description = encode(description);
-        }
 
         // Product images
         let images = [];
-        $('#additional-images img').map((i, e) => {
-            let src = e.attribs.src;
+        document.querySelectorAll('#additional-images img').forEach(el => {
+            let src = el.src;
             src = src.split('?')[0];
             images.push(url.slice(0, -1) + src);
         });
 
         if (images.length === 0) {
-            let src = $('#product-thumb-container img')[0].attribs.src;
+            let src = document.querySelectorAll('#product-thumb-container img')[0].src;
             src = src.split('?')[0];
             images.push(url.slice(0, -1) + src);
         }
 
-        // Product price
-        let price = $('#product-price').text();
-        if (price === '' || price == 0) {
-            price = null;
-        } else {
-            price = Number(price.replace(/[\$,]/g, ''));
+        // Product variations
+        let variations = [];
+        let reservationEl = document.querySelector('#Reservation');
+        let attributeListEl = reservationEl.querySelector('#AttributeList');
+        if (attributeListEl) {
+            let options = attributeListEl.querySelectorAll('option');
+            if (options && options.length > 0) {
+                options.forEach(el => {
+                    variations.push({
+                        text: el.getAttribute('data-subtext').replace(/\s\s+/g, '').trim(),
+                        value: Number(el.getAttribute('value')),
+                    });
+                });
+            }
+        }
+        if (variations.length === 0) {
+            // Product price
+            let price = document.querySelector('#product-price').textContent;
+            if (price === '') {
+                price = null;
+            } else {
+                price = Number(price.replace(/[\$,]/g, ''));
+                if (price === 0) {
+                    price = null;
+                }
+            }
+
+            // Product variation
+            let strongEls = reservationEl.querySelectorAll('strong');
+            if (strongEls && strongEls.length > 0) {
+                let text;
+                strongEls.forEach(el => {
+                    let lowerText = el.textContent.toLowerCase();
+                    if (lowerText.indexOf('dsrp') >= 0 || lowerText.indexOf('quantity') >= 0) {
+                        return;
+                    }
+                    if (!text) {
+                        text = el.textContent;
+                    }
+                });
+                variations.push({
+                    text: text || '',
+                    value: price,
+                });
+            } else {
+                console.log(strongEls);
+            }
         }
 
         // Product specs
         let specs = [];
-        $('table.specs-table-tab tbody tr').map((i, el) => {
-            el = cheerio.load(el);
+        document.querySelectorAll('table.specs-table-tab tbody tr').forEach(el => {
             let spec = {};
-            el('td').map((i, e) => {
-                let text = cheerio.load(e).text();
+            el.querySelectorAll('td').forEach((el, i) => {
+                let text = el.textContent;
                 if (i === 0) {
-                    if (encodeEntities) {
-                        spec.property = encode(text);
-                    } else {
-                        spec.property = text;
-                    }
+                    spec.property = text.trim();
                 } else {
-                    if (encodeEntities) {
-                        spec.data = encode(text);
-                    } else {
-                        spec.data = text;
-                    }
+                    spec.data = text.trim();
                 }
-            });
+            })
             specs.push(spec);
         });
 
+        // Product features
+        let features = [];
+        document.querySelectorAll('#features .feature').forEach(el => {
+            let descriptionEl = el.querySelector('.description');
+            let title = descriptionEl.querySelector('b') || descriptionEl.querySelector('strong');
+            if (title) {
+                title = title.textContent;
+                if (title.toLowerCase().indexOf('common features') >= 0) {
+                    return;
+                }
+            } else {
+                return;
+            }
+            let text = '';
+            descriptionEl.childNodes.forEach(el => {
+                if (el.textContent !== title) {
+                    text += el.textContent;
+                }
+            });
+            text = text.replace(/\s\s+/g, '');
+            let image = '';
+            el.querySelectorAll('img').forEach(el => {
+                let alt = el.getAttribute('alt');
+                if (!alt || alt.toLowerCase() !== 'zoom') {
+                    let src = el.getAttribute('data-original').split('?')[0];
+                    image = url.slice(0, -1) + src;
+                }
+            })
+            features.push({
+                title,
+                text,
+                image,
+            });
+        });
+
+        // Aggregate rating
+        let aggregateRating = {};
+        let aggregateRatingEl = document.querySelector('[itemprop=aggregateRating]');
+        if (aggregateRatingEl) {
+            aggregateRating.ratingValue = aggregateRatingEl.querySelector('[itemprop=ratingValue]').textContent;
+            aggregateRating.bestRating = aggregateRatingEl.querySelector('[itemprop=bestRating]').textContent;
+            aggregateRating.reviewCount = aggregateRatingEl.querySelector('[itemprop=reviewCount]').textContent;
+        }
+
+        if (encodeEntities) {
+            title = encode(title);
+            description = encode(description);
+            specs = specs.map(obj => {
+                obj.property = encode(obj.property);
+                obj.data = encode(obj.data);
+                return obj;
+            });
+            features = features.map(obj => {
+                obj.title = encode(obj.title);
+                obj.text = encode(obj.text);
+                return obj;
+            });
+        }
+
         return {
             title,
+            variations,
             sku,
             category,
             subcategory,
             description,
             images,
-            price,
             specs,
+            features,
+            aggregateRating,
             url: productUrl,
         };
     });
